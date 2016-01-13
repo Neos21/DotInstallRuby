94c9b005c2e8252312675620d732e1c20f846f4b
Neos21 <neos21@gmail.com>
2016-01-13 23:08:10 +0900

    初の Ruby コードです。

---
+++ Lesson1.rb
@@ -0,0 +1,20 @@
+=begin
+Lesson 1 : Hello World
+
+Ruby のことはじめ。
+・単一行コメントは「#」と書く。
+・複数行コメントは「=begin」と「=end」行で囲む。
+=end
+
+# print : 終わった後に改行がない
+print "Hello World!"
+
+# puts : 改行あり
+puts
+puts "Hello World (puts)"
+
+# p : データ形式が分かるように表示される
+p "Hello World (p)"
+
+# 「;」を書けば続けて処理が書ける。
+print "Hello "; print "World!"
